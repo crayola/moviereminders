3698f5d4a3843937277e451de700236e8dee2771
Timothee Carayol <timothee.carayol@gmail.com>
2012-10-14 08:14:02 +0100

    fixed template dir

--- moviepeople/settings.py
+++ moviepeople/settings.py
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'moviepeople.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'moviepeople.wsgi.application'
 
-TEMPLATE_DIRS = ('C:/python/moviepeople/templates',)
+TEMPLATE_DIRS = ('templates')
 
 INSTALLED_APPS = (
     'django.contrib.auth',
