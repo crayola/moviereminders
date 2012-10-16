0d463d9886b7dbae74cf7337cb46299dfeae4d83
Timothee Carayol <timothee.carayol@gmail.com>
2012-10-16 01:15:10 +0100

    separate parsing from writing to db, for stability & clarity. more refactoring to come soon.

--- APIparser/parser.py
+++ APIparser/parser.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.datetime_safe import date
 from moviepeopleapp.models import People, MoviePeople, Trailer, Release, Movie
-from urllib2 import urlopen, Request
+from urllib2 import urlopen, Request, URLError, HTTPError
 
 log = logging.getLogger(__name__)
 
@@ -16,7 +16,6 @@ def frontpage(request):
     return render(request,'frontpage.html',{'test':'test'})
 
 
-#http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=5z9vxrv4mrkcdkw2fbymhhdh&q=aca&page_limit=10
 apikey="3dffd9e01086f6801f45d2161cd2710d"
 
 def checkmovie(idmovie, info):
@@ -25,60 +24,83 @@ def checkmovie(idmovie, info):
     else: url="http://api.themoviedb.org/3/movie/%s/%s?api_key=%s" % (idmovie, info, apikey)
     request = Request(url)
     request.add_header("Accept", "application/json")
-    req = urlopen(request)
-    return simplejson.load(req)
+    read=0
+    while read==0:
+        try:
+            req = urlopen(request, timeout=3)
+            read=1
+        except HTTPError:
+            print "HTTP error."
+            raise
+        except URLError:
+            read=0
+            print "Timeout -- trying again."
+    parsed=0
+    while parsed==0:
+        try:
+            ret=simplejson.load(req)
+            parsed=1
+        except:
+            parsed=0
+            print "JSON issue, probably socket.timeout."
+    return ret
 
 
-#def queryMovies(searchquery, apikey=apikey):
-#    req = urlopen("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=%s&q=%s&page_limit=50" % (apikey, searchquery))
-#    return simplejson.load(req)
-
-#tmp=queryMovies("indiana").get('movies')
-
-#
-#def checkAPI():
-#    from urllib2 import urlopen
-#    from django.utils import simplejson
-#    req = urlopen("http://api.rottentomatoes.com/api/public/v1.0/movies/770672024.json?apikey=%s" % apikey)
-#    return simplejson.load(req)
-#
-
-#def parseMovie(movie):
-#    title=movie.get('title')
-#    tmp1=Movie.objects.filter(name=title)
-#    tmp2=Release.objects.filter(date=movie.get('release_dates').get('theater'))
-#    if (len([x.id for x in tmp1].intersect([y.movie_id for y in tmp2])) > 0):
-#        dbmovie=tmp[0]
-#    else:
-#        dbmovie=Movie(name=title)
-#    dbmovie.save()
-#    cast=movie.get('abridged_cast')
-#    people=[role.get('name') for role in cast]
-#    date=movie.get('release_dates').get('theater')
-#    if date==None: date="1970-01-01" # TODO: fix this!!
-#    Release(movie=dbmovie, date=date).save()
-#    for role in people:
-#        tmp=People.objects.filter(name=role)
-#        if len(tmp) > 0:
-#            dbpeople=tmp[0]
-#        else: 
-#            dbpeople=People(name=role)
-#        dbpeople.save()
-#        MoviePeople(people=dbpeople, movie=dbmovie, role="actor").save()
-#    # need to do director, but one API call per movie required :-/
-#    return [title,date,people]
 
 def parseMovie(tmdbid):
     try:
         movie_main=checkmovie(tmdbid, "main")
     except:
-        print "no movie for id %s" % tmdbid
+        print "id %s: no movie" % tmdbid
         return 0
     movie_cast=checkmovie(tmdbid, "casts")
     movie_release=checkmovie(tmdbid, "releases")
-    f=open("/home/ubuntu/dumptmdb", "a")
+    print "id %s: writing to disk" % tmdbid
+    f=open("/home/ubuntu/dumptmdb%s" % (tmdbid/10000), "a")
     simplejson.dump({"main": movie_main, "casts": movie_cast, "releases": movie_release}, f)
+    f.write("\n")
     f.close()
+    return 1
+    #title=movie_main['title']
+    #(poster, backdrop) = (movie_main['poster_path'], movie_main['backdrop_path'])
+    #if not(poster): poster=''
+    #if not(backdrop): backdrop=''
+    #dbmovie=Movie(name=title, poster=poster, backdrop=backdrop)
+    #dbmovie.save()
+    #date=[x['release_date'] for x in movie_release['countries'] if x['iso_3166_1']=='US']
+    #if date: Release(movie=dbmovie, date=date[0]).save()
+    #people=[x['name'] for x in movie_cast['cast']]
+    #director=[x['name'] for x in movie_cast['crew'] if x['job']=='Director']
+    #for role in people:
+    #    tmp=People.objects.filter(name=role)
+    #    if len(tmp) > 0:
+    #        dbpeople=tmp[0]
+    #    else: 
+    #        dbpeople=People(name=role)
+    #    dbpeople.save()
+    #    MoviePeople(people=dbpeople, movie=dbmovie, role="actor").save()
+    #for role in director:
+    #    #print director
+    #    tmp=People.objects.filter(name=role)
+    #    if len(tmp) > 0:
+    #        dbpeople=tmp[0]
+    #    else: 
+    #        dbpeople=People(name=role)
+    #    dbpeople.save()
+    #    MoviePeople(people=dbpeople, movie=dbmovie, role="director").save()
+    #return [title,date,people]
+
+
+
+def parseDump(dumpfile):
+    for movie in [simplejson.loads(line) for line in open(dumpfile)]:
+        movie_main=movie["main"]
+        movie_cast=movie["casts"]
+        movie_release=movie["releases"]
+        writeMovie(movie_main, movie_cast, movie_release)
+
+
+def writeMovie(movie_main, movie_cast, movie_release):
     title=movie_main['title']
     (poster, backdrop) = (movie_main['poster_path'], movie_main['backdrop_path'])
     if not(poster): poster=''
@@ -98,7 +120,7 @@ def parseMovie(tmdbid):
         dbpeople.save()
         MoviePeople(people=dbpeople, movie=dbmovie, role="actor").save()
     for role in director:
-        print director
+        #print director
         tmp=People.objects.filter(name=role)
         if len(tmp) > 0:
             dbpeople=tmp[0]
@@ -106,19 +128,4 @@ def parseMovie(tmdbid):
             dbpeople=People(name=role)
         dbpeople.save()
         MoviePeople(people=dbpeople, movie=dbmovie, role="director").save()
-    #for poster in [x['file_path'] for x in movie_images['posters']]:
-    #    Poster(url=poster, movie=dbmovie).save()
-    #for poster in [x['file_path'] for x in movie_images['backdrops']]:
-    #    Backdrop(url=poster, movie=dbmovie).save()
     return [title,date,people]
-
-#[parseMovie(x) for x in range(12, 100)]
-
-#def parseMoviesSearch(query):
-#    tmp=[parseMovie(movie) for movie in queryMovies(query).get('movies') if
-#	    all([movie.get('ratings').get('critics_score')>0, 
-#		movie.get('ratings').get('audience_score')>0])
-#	    ]
-#    return "Done query %s. Processed %i movies." % (query, len(tmp))
-#queryMovies('dark').get('movies')[0]
-
