69b64e91a7644596efc71440084c9aa76bee31c4
Timothee Carayol <timothee.carayol@gmail.com>
2013-02-01 17:52:21 +0000

    importance threshold is much lower

--- moviepeopleapp/search_indexes.py
+++ moviepeopleapp/search_indexes.py
@@ -11,7 +11,7 @@ class PeopleIndex(indexes.SearchIndex):
         return People
 
     def index_queryset(self):
-        return People.objects.filter(importance__gte=20)
+        return People.objects.filter(importance__gte=5)
 
 
 site.register(People, PeopleIndex)
